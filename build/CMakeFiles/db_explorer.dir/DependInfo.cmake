37932aed9d2d3a5dea08a596e4dc5798a3aff301
MDSAJ66 <ajmds66@gmail.com>
2025-12-20 02:40:47 -0600

    Updated the Readme with instructions and

---
+++ build/CMakeFiles/db_explorer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ajbamgbelu/Documents/DatabaseExplorer/src/cassandra_demo.cpp" "CMakeFiles/db_explorer.dir/src/cassandra_demo.cpp.o" "gcc" "CMakeFiles/db_explorer.dir/src/cassandra_demo.cpp.o.d"
+  "/Users/ajbamgbelu/Documents/DatabaseExplorer/src/main.cpp" "CMakeFiles/db_explorer.dir/src/main.cpp.o" "gcc" "CMakeFiles/db_explorer.dir/src/main.cpp.o.d"
+  "/Users/ajbamgbelu/Documents/DatabaseExplorer/src/mongodb_demo.cpp" "CMakeFiles/db_explorer.dir/src/mongodb_demo.cpp.o" "gcc" "CMakeFiles/db_explorer.dir/src/mongodb_demo.cpp.o.d"
+  "/Users/ajbamgbelu/Documents/DatabaseExplorer/src/neo4j_demo.cpp" "CMakeFiles/db_explorer.dir/src/neo4j_demo.cpp.o" "gcc" "CMakeFiles/db_explorer.dir/src/neo4j_demo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/db_explorer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db_explorer.dir/src/cassandra_demo.cpp.o"
+  "CMakeFiles/db_explorer.dir/src/cassandra_demo.cpp.o.d"
+  "CMakeFiles/db_explorer.dir/src/main.cpp.o"
+  "CMakeFiles/db_explorer.dir/src/main.cpp.o.d"
+  "CMakeFiles/db_explorer.dir/src/mongodb_demo.cpp.o"
+  "CMakeFiles/db_explorer.dir/src/mongodb_demo.cpp.o.d"
+  "CMakeFiles/db_explorer.dir/src/neo4j_demo.cpp.o"
+  "CMakeFiles/db_explorer.dir/src/neo4j_demo.cpp.o.d"
+  "db_explorer"
+  "db_explorer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db_explorer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
